77a2ad5c7ba7322d095a2ee11e3ae20be831068a
한현우 <hhw3422@gmail.com>
2025-02-05 09:21:07 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 21.27 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        for(int i=0; i<s; i++){
+            answer += my_string.charAt(i);
+        }
+        
+        answer += overwrite_string+my_string.substring(s+overwrite_string.length());
+        
+        return answer;
+    }
+}
