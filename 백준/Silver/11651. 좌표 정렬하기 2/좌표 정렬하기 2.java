499f0d01e952512cb6c9d093f97f492c01a75a14
한현우 <hhw3422@gmail.com>
2025-03-20 11:21:22 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 604 ms, Memory: 51600 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -21,18 +21,23 @@ public class Main {
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arr, new Comparator<int[]>() {
-			
-			@Override
-			public int compare(int []o1, int []o2) {
-				if(o1[1] == o2[1]) {
-					return o1[0] - o2[0];
-				}else {
-					return o1[1] - o2[1];
-				}
-			}
-			
-		});
+//		Arrays.sort(arr, new Comparator<int[]>() {
+//			
+//			@Override
+//			public int compare(int []o1, int []o2) {
+//				if(o1[1] == o2[1]) {
+//					return o1[0] - o2[0];
+//				}else {
+//					return o1[1] - o2[1];
+//				}
+//			}
+//			
+//		});
+		
+		Arrays.sort(arr, (p1, p2)->{
+            if(p1[1] == p2[1]) return p1[0] - p2[0];
+            else return p1[1] - p2[1];
+        });
 		
 		for(int i = 0; i < n; i++) {
 			sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
