c004063f94aa648085eff1c494137c35b762b050
한현우 <hhw3422@gmail.com>
2026-03-18 19:43:09 +0900

    [level 1] Title: 폰켓몬, Time: 5.84 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,18 +2,16 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int take = nums.length / 2;
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> setnums= new HashSet<>();
+        int count = nums.length / 2;
         
-        for(int num : nums){
-            set.add(num);
+        for(int i : nums){
+            setnums.add(i);
         }
         
-        if(set.size() >= take){
-            return take;
-        } 
-        else{
-            return set.size();
+        if(setnums.size() > count ){
+            return count;
         }
+        else return setnums.size();
     }
 }
