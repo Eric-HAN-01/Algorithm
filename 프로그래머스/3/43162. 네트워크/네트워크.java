58b085fc9a28472e76a70b98a55416b32187d8bc
한현우 <hhw3422@gmail.com>
2026-03-27 15:35:20 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,60 @@
+import java.util.*;
+
 class Solution {
-    //DFS
-    public int solution(int n, int[][] computers) {
+    //BFS
+    public int solution(int n, int[][] computers){
         int answer = 0;
         boolean[] visit = new boolean[n];
         
         for(int i = 0; i < n; i++){
             if(!visit[i]){
-                dfs(computers, i, visit);
+                bfs(computers, i, visit);
                 answer++;
             }
         }
         return answer;
     }
     
-    static private void dfs(int[][] computers, int i, boolean[] visit){
-        visit[i] = true;
+    void bfs(int[][] computers, int start, boolean[] visit){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visit[start] = true;
         
-        for(int j = 0; j < computers.length; j++){
-            if(computers[i][j] == 1 && !visit[j]){
-                dfs(computers, j, visit);
+        while(!q.isEmpty()){
+            int net = q.poll();
+            
+            for(int j = 0; j < computers.length; j++){
+                if(computers[net][j] == 1 && !visit[j]){
+                    visit[j] = true;
+                    q.add(j);
+                }
             }
         }
     }
+    
+//     //DFS
+//     public int solution(int n, int[][] computers) {
+//         int answer = 0;
+//         boolean[] visit = new boolean[n];
+        
+//         for(int i = 0; i < n; i++){
+//             if(!visit[i]){
+//                 dfs(computers, i, visit);
+//                 answer++;
+//             }
+//         }
+//         return answer;
+//     }
+    
+//     static private void dfs(int[][] computers, int i, boolean[] visit){
+//         visit[i] = true;
+        
+//         for(int j = 0; j < computers.length; j++){
+//             if(computers[i][j] == 1 && !visit[j]){
+//                 dfs(computers, j, visit);
+//             }
+//         }
+//     }
+    
+    
 }
