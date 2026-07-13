e41de4cf736aaca0af003a0d5f1dc5d639d26633
한현우 <hhw3422@gmail.com>
2026-07-14 02:03:45 +0900

    [level 3] Title: 단어 변환, Time: 0.19 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,31 +1,34 @@
 import java.util.*;
 
-class Solution { 
-    //dfs
-    private int answer = Integer.MAX_VALUE;
-    private boolean[] visited;
-
+class Solution {
+    //bfs
     public int solution(String begin, String target, String[] words) {
-        visited = new boolean[words.length];
-        dfs(begin, target, words, 0);
-        return answer == Integer.MAX_VALUE ? 0 : answer;
-    }
-
-    private void dfs(String current, String target, String[] words, int depth) {
-        if (current.equals(target)) {
-            answer = Math.min(answer, depth);
-            return;
-        }
-
-        for (int i = 0; i < words.length; i++) {
-            if (!visited[i] && diffCount(current, words[i]) == 1) {
-                visited[i] = true;
-                dfs(words[i], target, words, depth + 1);
-                visited[i] = false;
+        boolean[] visited = new boolean[words.length];
+        Queue<String> q = new ArrayDeque<>();
+        Queue<Integer> depthQ = new ArrayDeque<>();
+        q.offer(begin);
+        depthQ.offer(0);
+        
+        while (!q.isEmpty()) {
+            String current = q.poll();
+            int depth = depthQ.poll();
+            
+            if (current.equals(target)) {
+                return depth;
+            }
+            
+            for (int i = 0; i < words.length; i++) {
+                if (!visited[i] && diffCount(current, words[i]) == 1) {
+                    visited[i] = true;
+                    q.offer(words[i]);
+                    depthQ.offer(depth + 1);
+                }
             }
         }
+        
+        return 0;
     }
-
+        
     private int diffCount(String a, String b) {
         int count = 0;
         for (int i = 0; i < a.length(); i++) {
@@ -34,3 +37,38 @@ class Solution {
         return count;
     }
 }
+
+// class Solution { 
+//     //dfs
+//     private int answer = Integer.MAX_VALUE;
+//     private boolean[] visited;
+
+//     public int solution(String begin, String target, String[] words) {
+//         visited = new boolean[words.length];
+//         dfs(begin, target, words, 0);
+//         return answer == Integer.MAX_VALUE ? 0 : answer;
+//     }
+
+//     private void dfs(String current, String target, String[] words, int depth) {
+//         if (current.equals(target)) {
+//             answer = Math.min(answer, depth);
+//             return;
+//         }
+
+//         for (int i = 0; i < words.length; i++) {
+//             if (!visited[i] && diffCount(current, words[i]) == 1) {
+//                 visited[i] = true;
+//                 dfs(words[i], target, words, depth + 1);
+//                 visited[i] = false;
+//             }
+//         }
+//     }
+
+//     private int diffCount(String a, String b) {
+//         int count = 0;
+//         for (int i = 0; i < a.length(); i++) {
+//             if (a.charAt(i) != b.charAt(i)) count++;
+//         }
+//         return count;
+//     }
+// }
